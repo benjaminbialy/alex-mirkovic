021f47c50e98738b0375d87bfc1ef153edef428a
Benjamin Bialy <benjamincbialy@gmail.com>
2022-06-14 20:54:17 +1000

    added document

---
+++ pages/_document.js
@@ -0,0 +1,58 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta charset="utf-8" />
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          href="/public/apple-touch-icon-iphone-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/public/apple-touch-icon-ipad-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/public/apple-touch-icon-iphone-retina-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/public/apple-touch-icon-ipad-retina-152x152.png"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <script
+          src="https://www.paypal.com/sdk/js?client-id=AcmlbQggAMzJsVC2CxUrtsQUlXus3nHHckdxm63fC0cTjW-12jhPLf9yQS3UUKi9nPOF3HEFjUI76I1p&currency=AUD"
+          data-uid-auto="uid_jjrfqkrdjrrlxjyisejljfrdcclpzf"
+        ></script>
+        <meta
+          name="description"
+          content="Web site created using create-react-app"
+        />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+
+        <link
+          rel="manifest"
+          href="%PUBLIC_URL%/manifest.json"
+          crossorigin="use-credentials"
+        />
+
+        <title>Alex Mirkovic | VCE Tutor + Photographer</title>
+        <meta
+          name="description"
+          content="Alex Mirkovic is a VCE Tutor Located in Warragul, Victoria, Australia. Part of the Baw Baw Shire. Alex Mirkovic is also an experienced photographer. Alex Mirkovic tutors VCE Maths Methods, VCE Physics, VCE Specialist Mathematics, VCE English Language, VCE Chemistry and VCE Biology. Alex Mirkovic also sells VCE Educational Materials such as VCE Posters and VCE Notes."
+        />
+        <meta
+          name="google-site-verification"
+          content="8SqPcFVa09rAcs8icfD8c5BwdngirUuZ9qGPQjxWOS4"
+        />
+      </Head>
+    </Html>
+  );
+}
